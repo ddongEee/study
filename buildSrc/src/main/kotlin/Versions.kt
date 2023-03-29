9fe7b55d72f54f317b33eae3ee8b0e0071b392ae
Haksung Kim <kmhak@amazon.com>
2023-03-29 16:10:20 +0900

    [STUDY-0000] Applied "com.linecorp.build-recipe-plugin"

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -3,4 +3,5 @@ object Versions {
     const val spock = "2.3-groovy-4.0"
     const val opencsv = "5.7.1"
     const val jackson = "2.14.2"
+    const val lineRecipePlugin = "1.1.1"
 }
--- sandbox/build.gradle.kts
+++ sandbox/build.gradle.kts
@@ -1,24 +1,5 @@
-plugins {
-    // Apply the groovy plugin to also add support for Groovy (needed for Spock)
-    groovy
-    // Apply the java-library plugin for API and implementation separation.
-    `java-library`
-}
-
-repositories {
-    // Use Maven Central for resolving dependencies.
-    mavenCentral()
-}
-
 dependencies {
     implementation(project(":libs:opencsv"))
     implementation("com.opencsv:opencsv:${Versions.opencsv}")
     implementation("com.fasterxml.jackson.core:jackson-databind:${Versions.jackson}")
-    testImplementation("org.apache.groovy:groovy:${Versions.groovy}")
-    testImplementation("org.spockframework:spock-core:${Versions.spock}")
-}
-
-tasks.named<Test>("test") {
-    // Use JUnit Platform for unit tests.
-    useJUnitPlatform()
 }
