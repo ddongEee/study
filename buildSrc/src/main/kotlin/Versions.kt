6e7fb240f863e1cefb44323cceaa7dcdaae7f510
김학성 <twoday.me@gmail.com>
2023-03-29 17:15:10 +0900

    Study/lib jackson (#2)
    * [STUDY-0000] adding libs and sandbox module
    
    * [STUDY-0000] Applied "com.linecorp.build-recipe-plugin"
    
    * [STUDY-0000] Setting spring application
    
    ---------
    
    Co-authored-by: Haksung Kim <kmhak@amazon.com>

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -4,4 +4,8 @@ object Versions {
     const val opencsv = "5.7.1"
     const val jackson = "2.14.2"
     const val lineRecipePlugin = "1.1.1"
+    const val kotlin = "1.6.21"
+
+    const val springBoot = "2.7.3"
+    const val springDependencyManagementPlugin = "1.0.11.RELEASE"
 }
